4f73fcb5977cbe173c85d3f464214560bdd0ba3d
syahiraa110 <puterinursyahira59@gmail.com>
2024-01-11 09:27:31 +0700

    industri

---
+++ github.java
@@ -0,0 +1,17 @@
+public class github {
+    public static void main(String[] args) {
+    String nama= "syahira";
+    char jenisKelamin= 'P';
+    int usia= 16;
+    int tinggiBadan= 150;
+    float beratBadan= 46.4f;
+    System.out.println(nama);
+
+System.out.println("Nama:"+nama);
+System.out.println("Jenis Kelamin:"+jenisKelamin);
+System.out.println("Usia:"+usia);
+System.out.println("Tinggi Badan:"+tinggiBadan);
+System.out.println("Berat Badan:"+beratBadan);
+    
+}
+}
